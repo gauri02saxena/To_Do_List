8c64072ec80062bfaeb0792da22c45d3c53947c2
gauri02saxena <gauri.saxena7379@gmail.com>
2023-07-18 22:37:19 +0530

    Updated insert function'

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date=require(__dirname+"/date.js");
 const mongoose=require("mongoose");
 
 const app = express();
@@ -38,16 +37,30 @@ const item3= new Item({
   name : "Have breakfast"
 });
 
-//Inserting to DB
-const defaultIteams=[item1,item2,item3];
-Item.insertMany(defaultIteams)
-.then(()=> console.log("Succesfully added defaultItems to DB"))
-.catch((err)=>console.log(err));
-
 
 app.get("/", function (req, res) {
 
-  res.render("list", { listTitle: Today, newAddedItems: items });
+
+  //Reading data from DB
+  Item.find({})
+  .then((foundItems)=> {
+    if(foundItems.length===0){
+      //Inserting to DB
+        const defaultIteams=[item1,item2,item3];
+        Item.insertMany(defaultIteams)
+        .then(()=> console.log("Succesfully added defaultItems to DB"))
+        .catch((err)=>console.log(err));
+        res.redirect("/");
+
+    }
+    else{
+      res.render("list", { listTitle: "Today", newAddedItems: foundItems });
+
+    }
+  }).catch((err)=>console.log(err));
+ 
+  
+  
 });
 
 app.post("/", function (req, res) {
