24a8dc5aa2b89ec9d5dc7d75cd183cacabcce912
gauri02saxena <gauri.saxena7379@gmail.com>
2023-07-19 17:38:28 +0530

    Added functionality to create new lists

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
 const app = express();
-
 
 //setting app's view engine to ejs or telling the app to use ejs because by default express uses jade
 app.set("view engine", "ejs");
@@ -12,77 +11,107 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //use public folder for loading static files like css or images etc
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://127.0.0.1/todolistDB", { useNewUrlParser: true });
 
-mongoose.connect("mongodb://127.0.0.1/todolistDB", {useNewUrlParser:true});
-
-//Schema
-const itemsSchema= {
-  name: String
+//Items Schema
+const itemsSchema = {
+  name: String,
 };
 
-//Model
-const Item= mongoose.model("Item", itemsSchema);
+//Item Model
+const Item = mongoose.model("Item", itemsSchema);
 
-//Documents
-const item1= new Item({
-  name:"Wake up"
+//Item Documents
+const item1 = new Item({
+  name: "Wake up",
 });
 
-const item2= new Item({
-  name : "Take a bath"
+const item2 = new Item({
+  name: "Take a bath",
 });
 
-
-const item3= new Item({
-  name : "Have breakfast"
+const item3 = new Item({
+  name: "Have breakfast",
 });
 
+//List Schema
+const listSchema = {
+  name: String,
+  items: [itemsSchema],
+};
+
+const defaultItems = [item1, item2, item3];
+
+//List Model
+const List = mongoose.model("List", listSchema);
 
 app.get("/", function (req, res) {
-
-
   //Reading data from DB
   //foundItems is the DB data which is an array of objects
   Item.find({})
-  .then((foundItems)=> {
-    if(foundItems.length===0){
-      //Inserting to DB
-        const defaultIteams=[item1,item2,item3];
-        Item.insertMany(defaultIteams)
-        .then(()=> console.log("Succesfully added defaultItems to DB"))
-        .catch((err)=>console.log(err));
+    .then((foundItems) => {
+      if (foundItems.length === 0) {
+        //Inserting to DB
+
+        Item.insertMany(defaultItems)
+          .then(() => console.log("Succesfully added defaultItems to DB"))
+          .catch((err) => console.log(err));
         res.redirect("/");
+      } else {
+        res.render("list", { listTitle: "Today", newAddedItems: foundItems });
+      }
+    })
+    .catch((err) => console.log(err));
+});
 
-    }
-    else{
-      res.render("list", { listTitle: "Today", newAddedItems: foundItems });
+//Creating dynamic routes through express routing parameters
+app.get("/:customListName", function (req, res) {
+  const customListName = req.params.customListName;
 
-    }
-  }).catch((err)=>console.log(err));
- 
-  
-  
+  //Checking if the default list items are present in the new list or not
+  List.findOne({ name: customListName })
+    .then((foundList) => {
+      if (!foundList) {
+        //Create default list
+        //Using same items in the List model as Item model
+        const list = new List({
+          name: customListName,
+          items: defaultItems,
+        });
+
+        list.save();
+        //Reloading the page to display the list on the browser 
+        res.redirect("/"+ customListName);
+
+      } else {
+        //Show existing list 
+        res.render("list", {listTitle: foundList.name, newAddedItems: foundList.items});
+      }
+    })
+    .catch((err) => console.log(err));
 });
 
 app.post("/", function (req, res) {
   const itemName = req.body.newItem;
 
-  const newItem= new Item({
-    name : itemName
+  const newItem = new Item({
+    name: itemName,
   });
 
   newItem.save();
 
   res.redirect("/");
- 
 });
 
-app.post("/delete", function(req,res){
-  const checkedItemId=req.body.checkbox;
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
 
   Item.findByIdAndRemove(checkedItemId)
-  .then(()=>{console.log("Successfully deleted item"); res.redirect("/");})
-  .catch((err)=>console.log(err));
+    .then(() => {
+      console.log("Successfully deleted item");
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
 });
 
 app.listen(3000, function (req, res) {
