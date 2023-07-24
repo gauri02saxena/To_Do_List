c2678470cbc8c2b8a63effa2a3c5347c2833f4a5
gauri02saxena <gauri.saxena7379@gmail.com>
2023-07-24 20:54:11 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -147,12 +147,6 @@ app.post("/delete", function (req, res) {
 });
 
 
-// PORT
-// let port= process.env.PORT;
-// if(port==null || port=="")
-// {
-//   port=3000;
-// }
 
 app.listen(process.env.PORT || 3000, function (req, res) {
   console.log("Server is running at port 3000");
